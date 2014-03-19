0cfd26e7c6242d734623588497d93c1ae44db92c
Jeff <zip.github@goldsy.net>
2014-03-18 22:36:53 -0400

    Added const flag to variable record class.
     On branch p3_conversion
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   VarRec.C
    	modified:   VarRec.H

--- VarRec.C
+++ VarRec.C
@@ -2,11 +2,12 @@
 
 
 // Class ctor.
-VarRec::VarRec(string var_name, TypeRec *var_type)
+VarRec::VarRec(string var_name, TypeRec *var_type, bool const_flag)
 {
     _var_name = var_name;
     _var_type = var_type;
     _loop_counter_flag = false;
+    _const_flag = const_flag;
 }
 
 
@@ -28,7 +29,19 @@ bool VarRec::get_loop_counter_flag()
 }
 
 
+bool VarRec::is_const()
+{
+    return _const_flag;
+}
+
+
 void VarRec::set_loop_counter_flag(bool flag)
 {
     _loop_counter_flag = flag;
+}
+
+
+void VarRec::set_const_flag(bool flag)
+{
+    _const_flag = flag;
 }
