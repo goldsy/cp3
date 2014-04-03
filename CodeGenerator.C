a38bde0ea0c5662a68528c61de0ad90ed96f58d7
Jeff <zip.github@goldsy.net>
2014-04-02 22:36:54 -0400

    Update member names to use _ and added line num.
    Just a couple simple development updates.
    
     On branch p3_conversion
     Changes to be committed:
    
    	modified:   CodeGenerator.C
    	modified:   CodeGenerator.H

--- CodeGenerator.C
+++ CodeGenerator.C
@@ -9,20 +9,23 @@ using namespace std;
 // Class ctor
 CodeGenerator::CodeGenerator()
 {
+    _curr_line_num = 0;
 }
 
 
+// Class dtor.
 CodeGenerator::~CodeGenerator()
 {
     // Close the file.
-    if (target.is_open())
+    if (_target.is_open())
     {
-        target.close();
+        _target.close();
     }
 }
 
 
+// This method initializes the outfile.
 void CodeGenerator::init_out_file(char *target_filename)
 {
-    target.open(target_filename);
+    _target.open(target_filename);
 }
